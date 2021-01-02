ea42875b39b0035d606c07b9fe8fd5debb97d8a9
Nishanth Mane <nishanth.engg93@gmail.com>
2021-01-02 18:10:38 +0530

    Updates

---
+++ component---src-pages-404-js-287ad4a56ac0065ac0a7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{w2l6:function(e,n,t){"use strict";t.r(n);var o=t("q1tI"),i=t.n(o);n.default=function(){return i.a.createElement("div",null,"Nothing to see here move on!")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-287ad4a56ac0065ac0a7.js.map
